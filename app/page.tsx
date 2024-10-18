1b1257886913502f5187b4890db8afa34a9ad084
Grenish rai <107925840+Grenish@users.noreply.github.com>
2024-10-18 13:08:45 +0530

    chore: Add TestPage component to Home page to test the components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Browse from "./pages/Browse";
 import Hero from "./pages/Hero";
+import TestPage from "./pages/TestPage";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Browse />
+      <TestPage />
     </>
   );
 }
