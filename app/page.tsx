f93903dd8acf10fe41ba1f51ded7ad316f959454
Grenish rai <mrcoder2033d@gmail.com>
2025-11-16 16:07:40 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,27 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <h2 className="text-4xl font-semibold">This UI</h2>
-      <p>This UI is the comphrensive UI component you&apos;l love.</p>
+    <div className="flex flex-col items-center justify-center min-h-svh h-full bg-[url('/bg.png')] bg-cover bg-no-repeat bg-center relative">
+      <div className="pointer-events-none absolute inset-0 backdrop-blur-[1px] bg-[radial-gradient(ellipse,rgba(255,255,255,0)_0%,rgba(255,245,230,0.3)_45%,rgba(255,255,255,0.75)_100%)]" />
+      <div className="w-full min-h-96 relative flex flex-col items-center justify-start">
+        <h1 className="text-9xl font-semibold text-background font-adore">
+          ThisUI
+        </h1>
+        <p className="text-xl text-background max-w-2xl text-center">
+          Your home for modern, polished UI. Built on top of shadcn and shaped
+          into a smoother, more consistent system that helps you design and
+          build faster.
+        </p>
+        <div className="mt-5 flex items-center gap-2">
+          <Button variant={"outline"} size={"sm"}>
+            Get Started
+          </Button>
+          <Button variant={"ghost"} size={"sm"}>
+            View Components
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
